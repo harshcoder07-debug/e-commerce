a18b2bb3ffc652281b16cfedffbd04ff4a1133e5
harsh <harshcoder07@gmail.com>
2026-07-30 20:25:10 +0530

    home updtaed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:ui';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopit/bloc/Authbloc/auth_bloc.dart';
 import 'package:shopit/bloc/homenavbloc/bloc/home_nav_bloc_bloc.dart';
@@ -10,6 +7,7 @@ import 'package:shopit/bloc/homenavbloc/bloc/home_nav_bloc_event.dart';
 import 'package:shopit/bloc/homenavbloc/bloc/home_nav_bloc_state.dart';
 import 'package:shopit/repository/Authrepository.dart';
 import 'package:shopit/screens/Search/Search.dart';
+import 'package:shopit/screens/cart/Cart.dart';
 import 'package:shopit/screens/home/Home.dart';
 import 'package:shopit/screens/profile/setting.dart';
 import 'package:shopit/widgets/Authwrapper.dart';
@@ -47,7 +45,7 @@ class MerchantApp extends StatelessWidget {
 class Mainscreen extends StatelessWidget {
   const Mainscreen({super.key});
   // list of screens
-  static const List<Widget> _screens = [Home(), search(), Setting()];
+  static const List<Widget> _screens = [Home(), search(), cart(), Setting()];
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<HomeNavBlocBloc, Navigationstate>(
@@ -57,79 +55,99 @@ class Mainscreen extends StatelessWidget {
             index: navselectstate.selectedindex,
             children: _screens,
           ),
-          bottomNavigationBar: Container(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              border: Border(
-                top: BorderSide(
-                  color: Color(0xFFEAEAEA),
-                  width: 1,
-                ), // Optional top border line
-              ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: List.generate(3, (index) {
-                //selctiuon set
-                final bool isselected = navselectstate.selectedindex == index;
-                //icon seted
-                final IconData icon = index == 0
-                    ? Icons.home
-                    : index == 1
-                    ? Icons.search
-                    : Icons.person;
-                //lable set
-                final String label = index == 0
-                    ? 'Home'
-                    : index == 1
-                    ? 'Search'
-                    : 'Profile';
-                return GestureDetector(
-                  onTap: () {
-                    context.read<HomeNavBlocBloc>().add(tabchanged(index));
-                  },
-                  behavior: HitTestBehavior.opaque,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      AnimatedContainer(
-                        duration: Duration(milliseconds: 500),
-                        curve: Curves.easeIn,
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 8,
-                        ),
-                        decoration: BoxDecoration(
-                          color: isselected
-                              ? const Color.fromARGB(255, 54, 137, 238)
-                              : Colors.transparent,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Icon(
-                          icon,
-                          color: isselected
-                              ? const Color.fromARGB(255, 244, 244, 244)
-                              : Colors.grey,
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        label,
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: isselected
-                              ? FontWeight.bold
-                              : FontWeight.normal,
-                          color: isselected
-                              ? const Color(0xFF3F51D8)
-                              : Colors.grey,
-                        ),
-                      ),
-                    ],
+          bottomNavigationBar: Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10, bottom: 20),
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 10,
+                    blurStyle: BlurStyle.outer,
+                    color: Colors.blue,
+                    offset: Offset(3, 3),
                   ),
-                );
-              }),
+                ],
+                borderRadius: BorderRadius.circular(30),
+                color: Color.fromARGB(255, 253, 253, 253),
+                border: Border(
+                  top: BorderSide(
+                    color: Color(0xFFEAEAEA),
+                    width: 1,
+                  ), // Optional top border line
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: List.generate(4, (index) {
+                  //selctiuon set
+                  final bool isselected = navselectstate.selectedindex == index;
+                  //icon seted
+                  final IconData icon = index == 0
+                      ? Icons.home_outlined
+                      : index == 1
+                      ? Icons.search_outlined
+                      : index == 2
+                      ? Icons.shopping_cart
+                      : Icons.person;
+                  //lable set
+                  final String label = index == 0
+                      ? 'Home'
+                      : index == 1
+                      ? 'Search'
+                      : index == 2
+                      ? 'Cart'
+                      : 'Profile';
+                  return GestureDetector(
+                    onTap: () {
+                      context.read<HomeNavBlocBloc>().add(tabchanged(index));
+                    },
+                    behavior: HitTestBehavior.opaque,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        AnimatedContainer(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.easeIn,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 8,
+                          ),
+                          decoration: BoxDecoration(
+                            color: isselected
+                                ? const Color.fromARGB(255, 60, 128, 230)
+                                : Colors.transparent,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Column(
+                            children: [
+                              Icon(
+                                icon,
+                                color: isselected
+                                    ? const Color.fromARGB(255, 253, 253, 253)
+                                    : const Color.fromARGB(255, 0, 0, 0),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          label,
+                          style: TextStyle(
+                            fontSize: isselected ? 14 : 12,
+                            fontWeight: isselected
+                                ? FontWeight.bold
+                                : FontWeight.normal,
+                            color: isselected
+                                ? const Color(0xFF3F51D8)
+                                : const Color.fromARGB(255, 0, 0, 0),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+              ),
             ),
           ),
         );
--- lib/screens/home/Home.dart
+++ lib/screens/home/Home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,6 +7,135 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("Home page")));
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Scaffold(
+        appBar: AppBar(title: Text("Merchant", style: TextStyle(fontSize: 28))),
+        drawer: Drawer(),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // heading text here
+                Text(
+                  "Curated",
+                  style: TextStyle(fontSize: 39, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Excellence",
+                  style: TextStyle(
+                    fontSize: 27,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+
+                // image + stack blur container
+                SizedBox(
+                  height: 430,
+                  width: double.infinity,
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        height: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(24),
+                          image: const DecorationImage(
+                            image: AssetImage("images/hhomeimg.png"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+
+                      //postion or secon card over it
+                      Positioned(
+                        bottom: 20,
+                        left: 20,
+                        right: 20,
+
+                        child: ClipRRect(
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                            child: Container(
+                              height: 200,
+                              padding: EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(0.06),
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(
+                                  color: Colors.white.withOpacity(
+                                    0.15,
+                                  ), // Refractive borders
+                                  width: 1,
+                                ),
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Summer Series 2026",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: const Color.fromARGB(
+                                        255,
+                                        188,
+                                        180,
+                                        180,
+                                      ),
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 10),
+                                  Text(
+                                    "The Art of Slow\nLiving",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 20),
+                                  ElevatedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadiusGeometry.circular(15),
+                                      ),
+                                      backgroundColor: Colors.blue,
+                                      foregroundColor: Colors.white,
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 20,
+                                        vertical: 10,
+                                      ),
+                                    ),
+                                    child: Text(
+                                      "Explore More",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                  //new Arrival
+                                  
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
